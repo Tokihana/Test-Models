1d24c05e52c5395095fb4878b796de4e7392b047
Tokihana <sine.leika@gmail.com>
2024-03-22 12:53:23 +0800

    commit automatic lr search methods

--- hyper_param_test.py
+++ hyper_param_test.py
@@ -6,6 +6,7 @@ from data.build import build_loader
 from train import create_logger, build_optimizer, build_scheduler, build_criterion
 from utils import save_checkpoint, load_checkpoint, top1_accuracy, load_finetune_weights, compute_flop_params, throughput
 from model import create_model
+from main import train_one_epoch, validate
 
 def parse_option():
     parser = argparse.ArgumentParser()
@@ -21,7 +22,7 @@ def parse_option():
 class BatchSizeTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.model = model = create_model(args, config)
+        self.model = create_model(args, config)
         self.model.cuda()
         
     def test_batch_size(self):
@@ -37,7 +38,38 @@ class BatchSizeTests(unittest.TestCase):
             batch_res.append([batchsize, through, step_time])
             
         logger.info(batch_res)
-
+        
+class LRTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.model = create_model(args, config)
+        self.model.cuda()
+        self.train_loader, self.val_loader, self.mix_fn = build_loader(config)
+        self.criterion = build_criterion(config)
+        
+    def test_lr(self):
+        lr_list = [0.1**i for i in range(-8, 1)]
+        lr_res = []
+        logger.info(f'testing lr')
+        for lr in lr_list:
+            config.defrost()
+            config.TRAIN.BASE_LR = lr
+            config.freeze()
+            self.optimizer = build_optimizer(config, self.model)
+            self.lr_scheduler = build_scheduler(config, self.optimizer, len(self.train_loader))
+            max_acc = self._train_10epoch()
+            lr_res.append([lr, max_acc])
+        logger.info(lr_res)
+        
+    def _train_10epoch(self):
+        for epoch in range(config.TRAIN.START_EPOCH, config.TRAIN.EPOCHS):
+            train_one_epoch(config=config, model=self.model, data_loader=self.train_loader, epoch=epoch, mix_fn = self.mix_fn, criterion=self.criterion, optimizer=self.optimizer, lr_scheduler=self.lr_scheduler, logger=logger)
+            if val_loader is not None:
+                acc, loss = validate(config, self.model, self.val_loader, logger)
+                max_acc = max(max_acc, acc)
+            
+        return max_acc
+    
 if __name__ == '__main__':
     # make config
     args, config = parse_option()
