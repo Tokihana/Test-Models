81ed581fd0fae42131f4902e54ffa4b3400bb25c
Tokihana <sine.leika@gmail.com>
2024-04-05 16:19:37 +0800

    prepare repeat cls tests

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from model import create_model
 def parse_option():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--config', default='./config/yaml/AffectNet7_cls_200epoch_Reduce.yaml', type=str, help='path to config yaml')
+    parser.add_argument('--config', default='./config/yaml/RAF-DB_200epoch_Reduce_Scheduler.yaml', type=str, help='path to config yaml')
     parser.add_argument('--use-checkpoint', action='store_true', help="whether to use gradient checkpointing to save memory")
 
     args, unparsed = parser.parse_known_args()
