5da6d2df4cefb429698f72a84dfe5e636b1216de
Tokihana <sine.leika@gmail.com>
2024-04-10 23:02:45 +0800

    fix LabelSmoothing issues

--- train/criterion.py
+++ train/criterion.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 
 def build_criterion(config):
@@ -11,3 +12,21 @@ def build_criterion(config):
     elif config.TRAIN.CRITERION.NAME == 'CrossEntropy':
         criterion = torch.nn.CrossEntropyLoss()
     return criterion
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    """ NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        assert smoothing < 1.0
+        self.smoothing = smoothing
+        self.confidence = 1. - smoothing
+
+    def forward(self, x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        logprobs = F.log_softmax(x, dim=-1)
+        # nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1).long()) # gather() requires index as type int64, so we need to true target to long int type
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
